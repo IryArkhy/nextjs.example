379595806807ae82c69ed60bde5b87f1930e01c7
IryArkhy <37odri@gmail.com>
2020-07-13 20:47:45 +0300

    final ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,47 +8,61 @@ import Link from 'next/link'
 import Date from '../components/date'
 
 //route url: '/'
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle} < /title>
-    < /Head>
-    < section className={utilStyles.headingMd} >
-            <p>Front - end Developer < /p>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Front-end developer</p>
         <p>
-                (This is a sample website - you’ll be building a site like this on{' '}
-                < a href="https://nextjs.org/learn" > our Next.js tutorial < /a>.)
-          < /p>
-          < /section>
-        < section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                    <h2 className={utilStyles.headingLg}> Blog < /h2>
-            < ul className={utilStyles.list} >
-                        {
-                          allPostsData.map(({ id, date, title }) => (
-                            <li className={utilStyles.listItem} key={id} >
-                              <Link href="/posts/[id]" as={`/posts/${id}`} >
-                                <a>{title} < /a>
-              < /Link>
-              < br />
-                                  <small className={utilStyles.lightText}>
-                                    <Date dateString={date} />
-                                  </small>
-                  </li>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
-</ul>
-  </section>
-  </Layout>
+        </ul>
+      </section>
+    </Layout>
   )
 }
 
 
-export const getStaticProps: GetStaticProps = async (context) =>{
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData
+//     }
+//   }
+// }
+
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
-                        props: {
-                        allPostsData
-                      }
+    props: {
+      allPostsData
+    }
   }
 }
 
